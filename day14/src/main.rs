d2e2abba133740aaf867b2504ed60561a47dcb3e
Brett Seton <brettseton@gmail.com>
2024-01-14 00:22:51 +1100

    day 14 part 2

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -17,19 +17,20 @@ fn main() {
 fn part1(file_path: &str) -> usize {
     let input = fs::read_to_string(file_path).expect("file input");
     let platform = Platform::new(&input);
-    return platform.get_load();
+    return platform.get_load_after_tilt();
 }
 
 fn part2(file_path: &str) -> usize {
     let input = fs::read_to_string(file_path).expect("file input");
-    let platform = Platform::new(&input);
-    return platform.get_load();
+    let mut platform = Platform::new(&input);
+    return platform.cycle_n(1_000_000_000);
 }
 
 struct Platform {
     map: Vec<Vec<char>>,
     width: usize,
-    height: usize
+    height: usize,
+    map_history: Vec<String>,
 }
 
 impl Platform {
@@ -37,16 +38,18 @@ impl Platform {
         return Platform::from_str(str).expect("");
     }
 
-    pub fn get_load(&self) -> usize {
+    pub fn get_load_after_tilt(&self) -> usize {
         let mut load = 0;
         for y in 0..self.height {
-            load += (0..self.width).filter_map(|x| self.find_resting_place(x, y)).sum::<usize>();
+            load += (0..self.width)
+                .filter_map(|x| self.find_resting_place(x, y))
+                .sum::<usize>();
         }
 
         return load;
     }
 
-    pub fn find_resting_place(&self, x: usize, y: usize) -> Option<usize>{
+    pub fn find_resting_place(&self, x: usize, y: usize) -> Option<usize> {
         if self.map[y][x] != 'O' {
             return None;
         }
@@ -59,7 +62,7 @@ impl Platform {
 
         for y_next in (0..y).rev() {
             if self.map[y_next][x] == '.' {
-                resting_height -= 1; 
+                resting_height -= 1;
             }
             if self.map[y_next][x] == '#' {
                 break;
@@ -68,23 +71,124 @@ impl Platform {
 
         return Some(self.height - resting_height);
     }
+
+    fn cycle_n(&mut self, n: usize) -> usize {
+        self.cycle();
+        let state = self.to_string();
+
+        // If we have already been in this state we have entered a cycle
+        // meaning we can now calculate what state will be the end state
+        match self
+            .map_history
+            .iter()
+            .enumerate()
+            .find(|(_, x)| *x == &state)
+        {
+            Some((index, _)) => self.map_history
+                [index + (n - 1) % (self.map_history.len() - index)]
+                .parse::<Platform>()
+                .expect("should be a platform")
+                .get_load(),
+            _ => {
+                self.map_history.push(self.to_string());
+                self.cycle_n(n - 1)
+            }
+        }
+    }
+
+    fn cycle(&mut self) {
+        // North -> West -> South -> East
+        self.tilt_y(0, 1);
+        self.tilt_x(0, 1);
+        self.tilt_y(self.height - 1, -1);
+        self.tilt_x(self.width - 1, -1);
+    }
+
+    fn tilt_y(&mut self, start_row_index: usize, delta: i64) {
+        for x in 0..self.width {
+            let mut rock_stack_start_y = start_row_index as i64;
+            let mut y: i64 = start_row_index as i64;
+
+            while y >= 0 && y < self.height as i64 {
+                match self.map[y as usize][x] {
+                    '.' => (),
+                    '#' => rock_stack_start_y = y + delta,
+                    'O' => {
+                        self.map[y as usize][x] = '.';
+                        self.map[rock_stack_start_y as usize][x] = 'O';
+                        rock_stack_start_y += delta;
+                    }
+                    _ => panic!("invalid"),
+                }
+
+                y += delta;
+            }
+        }
+    }
+
+    fn tilt_x(&mut self, start_col_index: usize, delta: i64) {
+        for y in 0..self.height {
+            let mut rock_stack_start_x = start_col_index as i64;
+            let mut x = start_col_index as i64;
+
+            while x >= 0 && x < self.width as i64 {
+                match self.map[y][x as usize] {
+                    '.' => (),
+                    '#' => rock_stack_start_x = x + delta,
+                    'O' => {
+                        self.map[y][x as usize] = '.';
+                        self.map[y][rock_stack_start_x as usize] = 'O';
+                        rock_stack_start_x += delta;
+                    }
+                    _ => panic!("invalid"),
+                }
+
+                x += delta;
+            }
+        }
+    }
+
+    fn get_load(&self) -> usize {
+        self.map
+            .iter()
+            .enumerate()
+            .map(|(i, row)| (self.width - i) * row.iter().filter(|&&x| x == 'O').count())
+            .sum()
+    }
 }
+
 
 #[derive(Debug)]
 struct PlatformError;
 
 impl FromStr for Platform {
     type Err = PlatformError;
-
+    
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let width = s.lines().nth(0).unwrap().len();
         let height = s.lines().count();
-        let map = s.lines().map(|s| s.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
-        return Ok(Platform { map, width, height });
+        let map = s
+        .lines()
+        .map(|s| s.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+        return Ok(Platform {
+            map,
+            width,
+            height,
+            map_history: Vec::from([s.to_string()]),
+        });
     }
 }
 
-
+impl ToString for Platform {
+    fn to_string(&self) -> String {
+        self.map
+            .iter()
+            .map(|row| row.iter().collect::<String>())
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+}
 
 #[test]
 pub fn part1_test1() {
@@ -101,11 +205,11 @@ pub fn part1_test2() {
 #[test]
 pub fn part2_test1() {
     let ans = part2("input/test1.txt");
-    assert_eq!(ans, 0);
+    assert_eq!(ans, 64);
 }
 
 #[test]
 pub fn part2_test2() {
     let ans = part2("input/test2.txt");
-    assert_eq!(ans, 0);
+    assert_eq!(ans, 106390);
 }
