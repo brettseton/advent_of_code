a78d105cc2f70ab9c385c9dfca6ebb9404a49b94
Brett Seton <brettseton@gmail.com>
2024-12-15 13:00:45 +1100

    apply fmt changes to day01

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -3,26 +3,24 @@ use std::fs;
 fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
     let mut left = Vec::new();
     let mut right = Vec::new();
-    
+
     for line in input.lines() {
-        let mut numbers = line
-            .split_whitespace()
-            .map(|n| n.parse::<i32>().unwrap());
-        
+        let mut numbers = line.split_whitespace().map(|n| n.parse::<i32>().unwrap());
+
         left.push(numbers.next().unwrap());
         right.push(numbers.next().unwrap());
     }
-    
+
     (left, right)
 }
 
 fn part1(input: &str) -> i32 {
     let (mut left, mut right) = parse_input(input);
-    
+
     // Sort both lists
     left.sort();
     right.sort();
-    
+
     // Calculate total distance between paired numbers
     left.iter()
         .zip(right.iter())
@@ -32,7 +30,7 @@ fn part1(input: &str) -> i32 {
 
 fn part2(input: &str) -> i32 {
     let (left, right) = parse_input(input);
-    
+
     // Calculate similarity score
     left.iter()
         .map(|&num| {
@@ -45,8 +43,10 @@ fn part2(input: &str) -> i32 {
 }
 
 fn main() {
-    let input1 = fs::read_to_string("input/test1.txt").expect("Should have been able to read the file");
-    let input2 = fs::read_to_string("input/test2.txt").expect("Should have been able to read the file");
+    let input1 =
+        fs::read_to_string("input/test1.txt").expect("Should have been able to read the file");
+    let input2 =
+        fs::read_to_string("input/test2.txt").expect("Should have been able to read the file");
     println!("Part 1 test 1: {}", part1(&input1));
     println!("Part 1 test 2: {}", part1(&input2));
 
@@ -60,25 +60,29 @@ mod tests {
 
     #[test]
     fn test1_part1() {
-        let test_input = fs::read_to_string("input/test1.txt").expect("Should have been able to read the file");
+        let test_input =
+            fs::read_to_string("input/test1.txt").expect("Should have been able to read the file");
         assert_eq!(part1(&test_input), 11);
     }
 
     #[test]
     fn test2_part1() {
-        let test_input = fs::read_to_string("input/test2.txt").expect("Should have been able to read the file");
+        let test_input =
+            fs::read_to_string("input/test2.txt").expect("Should have been able to read the file");
         assert_eq!(part1(&test_input), 2086478);
     }
 
     #[test]
     fn test1_part2() {
-        let test_input = fs::read_to_string("input/test1.txt").expect("Should have been able to read the file");
+        let test_input =
+            fs::read_to_string("input/test1.txt").expect("Should have been able to read the file");
         assert_eq!(part2(&test_input), 31);
     }
 
     #[test]
     fn test2_part2() {
-        let test_input = fs::read_to_string("input/test2.txt").expect("Should have been able to read the file");
+        let test_input =
+            fs::read_to_string("input/test2.txt").expect("Should have been able to read the file");
         assert_eq!(part2(&test_input), 24941624);
     }
-} 
+}
