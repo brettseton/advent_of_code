f832b847fabdc431df3089e428717e0f36fcd345
Brett Seton <brettseton@gmail.com>
2025-12-05 22:54:47 +1100

    day 1 2022

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,12 +1,28 @@
 const TEST_INPUT_1: &str = include_str!("../input/test1.txt");
 const TEST_INPUT_2: &str = include_str!("../input/test2.txt");
 
-fn part1(_input: &str) -> i32 {
-    0
+fn parse_sorted_calories(input: &str) -> Vec<i32> {
+    let mut calories: Vec<i32> = input
+        .replace("\r\n", "\n")
+        .split("\n\n")
+        .map(|elf_inventory| {
+            elf_inventory
+                .lines()
+                .map(|line| line.parse::<i32>().unwrap_or(0))
+                .sum()
+        })
+        .collect();
+
+    calories.sort_unstable_by(|a, b| b.cmp(a));
+    calories
 }
 
-fn part2(_input: &str) -> i32 {
-    0
+fn part1(input: &str) -> i32 {
+    parse_sorted_calories(input).first().copied().unwrap_or(0)
+}
+
+fn part2(input: &str) -> i32 {
+    parse_sorted_calories(input).iter().take(3).sum()
 }
 
 fn main() {
@@ -23,21 +39,21 @@ mod tests {
 
     #[test]
     fn test1_part1() {
-        assert_eq!(part1(TEST_INPUT_1), 0);
+        assert_eq!(part1(TEST_INPUT_1), 24000);
     }
 
     #[test]
     fn test2_part1() {
-        assert_eq!(part1(TEST_INPUT_2), 0);
+        assert_eq!(part1(TEST_INPUT_2), 69501);
     }
 
     #[test]
     fn test1_part2() {
-        assert_eq!(part2(TEST_INPUT_1), 0);
+        assert_eq!(part2(TEST_INPUT_1), 45000);
     }
 
     #[test]
     fn test2_part2() {
-        assert_eq!(part2(TEST_INPUT_2), 0);
+        assert_eq!(part2(TEST_INPUT_2), 202346);
     }
 }
