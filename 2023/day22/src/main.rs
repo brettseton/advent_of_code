c2f5e8f821eb5ba47cb87e70a75e7b12149c32ab
Brett Seton <brettseton@gmail.com>
2025-12-19 15:09:47 +1100

    improve the performance of day 22 2023

--- 2023/day22/src/main.rs
+++ 2023/day22/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashMap, HashSet, VecDeque},
+    collections::{HashSet, VecDeque},
     fs,
     str::FromStr,
 };
@@ -32,57 +32,43 @@ fn part2(file_path: &str) -> usize {
 
 #[derive(Debug, Clone)]
 struct Brick {
-    id: usize,
     start_point: Point3D,
     end_point: Point3D,
-    resting_on: HashSet<usize>,
 }
 
 impl Brick {
-    fn drop(&mut self, occupancy_grid: &mut [Vec<Vec<Option<usize>>>]) {
-        let min_x = self.start_point.x.min(self.end_point.x);
-        let max_x = self.start_point.x.max(self.end_point.x);
-        let min_y = self.start_point.y.min(self.end_point.y);
-        let max_y = self.start_point.y.max(self.end_point.y);
-        let min_z = self.start_point.z.min(self.end_point.z);
-        let max_z = self.start_point.z.max(self.end_point.z);
-        let block_height = max_z - min_z + 1;
+    fn x_range(&self) -> std::ops::RangeInclusive<usize> {
+        self.start_point.x.min(self.end_point.x)..=self.start_point.x.max(self.end_point.x)
+    }
 
-        for z in (1..self.start_point.z).rev() {
-            for y in min_y..=max_y {
-                for x in min_x..=max_x {
-                    if let Some(id) = occupancy_grid[z][y][x] {
-                        //println!("collided with {} resting of {:?} at height: {}", id, self, z+1);
-                        self.resting_on.insert(id);
-                        // Find any other blocks this is resting on
-                        for row in &occupancy_grid[z][min_y..=max_y] {
-                            for id in row[min_x..=max_x].iter().flatten() {
-                                self.resting_on.insert(*id);
-                            }
-                        }
-                        // Fill the occupancy grid for the other blocks to use
-                        for dz in 1..=block_height {
-                            for row in &mut occupancy_grid[z + dz][min_y..=max_y] {
-                                for cell in &mut row[min_x..=max_x] {
-                                    *cell = Some(self.id);
-                                }
-                            }
-                        }
-                        return;
-                    }
-                }
-            }
-        }
+    fn y_range(&self) -> std::ops::RangeInclusive<usize> {
+        self.start_point.y.min(self.end_point.y)..=self.start_point.y.max(self.end_point.y)
+    }
 
-        // no collisions and resting on the ground
-        //println!("resting on the ground {:?}", self);
-        for dz in occupancy_grid.iter_mut().take(block_height + 1).skip(1) {
-            for y in dz.iter_mut().take(max_y + 1).skip(min_y) {
-                for x in y.iter_mut().take(max_x + 1).skip(min_x) {
-                    *x = Some(self.id);
-                }
-            }
-        }
+    fn min_z(&self) -> usize {
+        self.start_point.z.min(self.end_point.z)
+    }
+
+    fn max_z(&self) -> usize {
+        self.start_point.z.max(self.end_point.z)
+    }
+
+    fn height(&self) -> usize {
+        self.max_z() - self.min_z() + 1
+    }
+}
+
+impl FromStr for Brick {
+    type Err = Point3DErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.split('~');
+        let start_point = parts.next().ok_or(Point3DErr)?.parse()?;
+        let end_point = parts.next().ok_or(Point3DErr)?.parse()?;
+        Ok(Brick {
+            start_point,
+            end_point,
+        })
     }
 }
 
@@ -124,81 +110,74 @@ impl BrickStack {
     }
 
     pub fn get_disintegration_count_and_fallen_sum(&self) -> (usize, usize) {
-        let mut bricks_ordered = self.bricks.clone();
-        bricks_ordered.sort_by_key(|b| b.start_point.z.min(b.end_point.z));
+        let mut bricks = self.bricks.clone();
+        bricks.sort_by_key(|b| b.min_z());
 
-        let max_x = self
-            .bricks
-            .iter()
-            .map(|b| b.start_point.x.max(b.end_point.x) + 1)
-            .max()
-            .unwrap();
-        let max_y = self
-            .bricks
-            .iter()
-            .map(|b| b.start_point.y.max(b.end_point.y) + 1)
-            .max()
-            .unwrap();
+        let max_x = bricks.iter().map(|b| *b.x_range().end()).max().unwrap_or(0);
+        let max_y = bricks.iter().map(|b| *b.y_range().end()).max().unwrap_or(0);
 
-        let mut occupancy_grid: Vec<Vec<Vec<Option<usize>>>> =
-            vec![vec![vec![None; max_x]; max_y]; bricks_ordered.last().unwrap().end_point.z];
-        let mut parent_of: HashMap<usize, Vec<(usize, usize)>> = HashMap::new();
+        let mut height_map = vec![vec![(0, None); max_x + 1]; max_y + 1];
+        let mut supported_by: Vec<HashSet<usize>> = vec![HashSet::new(); bricks.len()];
+        let mut supports: Vec<HashSet<usize>> = vec![HashSet::new(); bricks.len()];
 
-        for brick in bricks_ordered.iter_mut() {
-            brick.drop(&mut occupancy_grid);
+        for i in 0..bricks.len() {
+            let b = &bricks[i];
+            let x_range = b.x_range();
+            let y_range = b.y_range();
+            let height = b.height();
 
-            parent_of.insert(brick.id, vec![]);
-            for &resting_on in brick.resting_on.iter() {
-                parent_of
-                    .entry(resting_on)
-                    .or_default()
-                    .push((brick.id, brick.resting_on.len()));
+            let mut max_h = 0;
+            for y in y_range.clone() {
+                for x in x_range.clone() {
+                    max_h = max_h.max(height_map[y][x].0);
+                }
+            }
+
+            let new_top_h = max_h + height;
+            for y in y_range {
+                for x in x_range.clone() {
+                    if let (h, Some(prev_idx)) = height_map[y][x] {
+                        if h == max_h {
+                            supported_by[i].insert(prev_idx);
+                            supports[prev_idx].insert(i);
+                        }
+                    }
+                    height_map[y][x] = (new_top_h, Some(i));
+                }
             }
         }
 
-        let mut disintegrate = vec![];
-        let mut dont_disintegrate = vec![];
-        for (k, v) in parent_of.iter() {
-            if v.is_empty() || v.iter().all(|x| x.1 > 1) {
-                disintegrate.push(k);
-            } else {
-                dont_disintegrate.push(k);
+        let mut can_disintegrate_count = 0;
+        for i in 0..bricks.len() {
+            if supports[i]
+                .iter()
+                .all(|&supported_idx| supported_by[supported_idx].len() > 1)
+            {
+                can_disintegrate_count += 1;
             }
         }
 
         let mut fallen_sum = 0;
-        for &parent_id in dont_disintegrate.iter() {
-            let mut have_fallen: HashSet<usize> = HashSet::new();
-            have_fallen.insert(*parent_id);
+        for i in 0..bricks.len() {
+            let mut q = VecDeque::new();
+            q.push_back(i);
 
-            let mut might_fall: VecDeque<usize> = parent_of
-                .get(parent_id)
-                .unwrap()
-                .iter()
-                .map(|&x| x.0)
-                .collect();
+            let mut removed_supports = vec![0; bricks.len()];
+            let mut fallen_count = 0;
 
-            while let Some(c) = might_fall.pop_front() {
-                //check the bricks below and if all the blocks it is resting on have fallen add it to the list
-                let brick = bricks_ordered.iter().find(|x| x.id == c).unwrap();
-                if !brick.resting_on.iter().all(|x| have_fallen.contains(x)) {
-                    continue;
-                }
-
-                have_fallen.insert(c);
-
-                // Add the blocks above to be checked
-                for b in parent_of.get(&c).unwrap().iter().map(|&x| x.0) {
-                    if !have_fallen.contains(&b) {
-                        might_fall.push_back(b);
+            while let Some(curr) = q.pop_front() {
+                for &next in &supports[curr] {
+                    removed_supports[next] += 1;
+                    if removed_supports[next] == supported_by[next].len() {
+                        fallen_count += 1;
+                        q.push_back(next);
                     }
                 }
             }
-
-            fallen_sum += have_fallen.len() - 1;
+            fallen_sum += fallen_count;
         }
 
-        (disintegrate.len(), fallen_sum)
+        (can_disintegrate_count, fallen_sum)
     }
 }
 
@@ -211,20 +190,7 @@ impl FromStr for BrickStack {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let bricks: Vec<Brick> = s
             .lines()
-            .enumerate()
-            .map(|(i, s)| {
-                let [start_str, end_str] =
-                    &s.split('~').map(String::from).collect::<Vec<String>>()[..]
-                else {
-                    panic!()
-                };
-                Brick {
-                    id: i,
-                    start_point: start_str.parse::<Point3D>().expect(""),
-                    end_point: end_str.parse::<Point3D>().expect(""),
-                    resting_on: HashSet::new(),
-                }
-            })
+            .map(|s| s.parse().expect("Failed to parse brick"))
             .collect();
 
         Ok(BrickStack { bricks })
