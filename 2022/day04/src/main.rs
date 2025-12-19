6229e00bc95db9523f37ec5aa28358e3acd6d459
Brett Seton <brettseton@gmail.com>
2025-12-19 17:10:12 +1100

    day 04 2022

--- 2022/day04/src/main.rs
+++ 2022/day04/src/main.rs
@@ -1,12 +1,48 @@
 const TEST_INPUT_1: &str = include_str!("../input/test1.txt");
 const TEST_INPUT_2: &str = include_str!("../input/test2.txt");
 
-fn part1(_input: &str) -> i32 {
-    0
+struct Range {
+    start: i32,
+    end: i32,
 }
 
-fn part2(_input: &str) -> i32 {
-    0
+impl Range {
+    fn from_str(s: &str) -> Self {
+        let parts: Vec<&str> = s.split('-').collect();
+        Range {
+            start: parts[0].parse().unwrap(),
+            end: parts[1].parse().unwrap(),
+        }
+    }
+
+    fn contains(&self, other: &Range) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+
+    fn overlaps(&self, other: &Range) -> bool {
+        self.start <= other.end && self.end >= other.start
+    }
+}
+
+fn part1(input: &str) -> i32 {
+    parse_pairs(input)
+        .filter(|(r1, r2)| r1.contains(r2) || r2.contains(r1))
+        .count() as i32
+}
+
+fn part2(input: &str) -> i32 {
+    parse_pairs(input)
+        .filter(|(r1, r2)| r1.overlaps(r2))
+        .count() as i32
+}
+
+fn parse_pairs(input: &str) -> impl Iterator<Item = (Range, Range)> + '_ {
+    input.lines().filter(|line| !line.is_empty()).map(|line| {
+        let ranges: Vec<&str> = line.split(',').collect();
+        let r1 = Range::from_str(ranges[0]);
+        let r2 = Range::from_str(ranges[1]);
+        (r1, r2)
+    })
 }
 
 fn main() {
@@ -23,21 +59,21 @@ mod tests {
 
     #[test]
     fn test1_part1() {
-        assert_eq!(part1(TEST_INPUT_1), 0);
+        assert_eq!(part1(TEST_INPUT_1), 2);
     }
 
     #[test]
     fn test2_part1() {
-        assert_eq!(part1(TEST_INPUT_2), 0);
+        assert_eq!(part1(TEST_INPUT_2), 540);
     }
 
     #[test]
     fn test1_part2() {
-        assert_eq!(part2(TEST_INPUT_1), 0);
+        assert_eq!(part2(TEST_INPUT_1), 4);
     }
 
     #[test]
     fn test2_part2() {
-        assert_eq!(part2(TEST_INPUT_2), 0);
+        assert_eq!(part2(TEST_INPUT_2), 872);
     }
 }
